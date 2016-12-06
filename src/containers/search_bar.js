dc675e1eacbe3b0d856c81c947a34d3cfa21308a
Preston Edmands <pedmands@gmail.com>
2016-12-06 04:07:53 -0500

    created onInputChange method and bound this to SearchBar container

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+
+    this.onInputChange = this.onInputChange.bind(this);
+    // binds this to onInputChange function, below,
+    // thus overriding the this.setState method in the function
+  }
+
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
+
   render(){
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Search for a city to see a five-day forecast..."
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-info">Submit</button>
         </span>
