91a97e27703b41c265a3a092d625820a2bac1b1d
Preston Edmands <pedmands@gmail.com>
2016-12-06 04:59:03 -0500

    Set up API url and installed axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,13 @@
 const API_KEY = '38895acdfbb000a465aa7ccad31b04ab';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 // Action Creators
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  return {
+    type: FETCH_WEATHER
+  };
+}
